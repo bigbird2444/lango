3bb450d775721daf7ee8212fbc33207db95f464d
bigbird2444 <bigbird2444@sina.com>
2014-11-16 17:37:56 +0800

    加法完成
    完成加法算法

--- lango.c
+++ lango.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int transtr(char* str);
+int gettype(char c);
+int chack(char* str);
+int tonum(char* str);
 
 int main()
 {
@@ -26,38 +29,88 @@ int main()
 int transtr(char* str){
     char* str1=str;
     char table[20][8];
+    int sta_table[20]; 
     int i=0,j=0,n;
-    int sta=0,sta0=0;
+    int sta=0,sta0;//×ÖÄ¸×´Ì¬0ÎªÊı×Ö 1Îª·ûºÅ 
+    int sum=0;    
 
-    //while(*str1!='\0'){
-       // if((*str1>'9')||(*str1<'('))
-         //   return 1; 
-      // str1++; 
-    //} 
-
-    //str1=str;
+    if(chack(str))
+    return 1;
+    	
+    while(*str1==' ')
+        str1++;
+    sta0=gettype(*str1);
     while(*str1!='\0'){
-        sta=(*str1>='0')?0:1;
-
+        sta=gettype(*str1);
+        if(*str1!=' '){       
         if(sta!=sta0){
-                
                 table[i][j]='\0';
                 i++;
                 j=0;
         }
-        if(*str1!=' ')
-        table[i][j]=*str1;
-        j++;
+       
+                table[i][j]=*str1;
+                sta_table[i]=sta;
+                j++;
+        }
         str1++;
-
         sta0=sta;
     }
     table[i][j]='\0';
 
+    //for(n=0;n<=i;n++)                  
+    //    printf("%s ",table[n]);
+   // printf("%d\n",i+1);
+    
+    //for(n=0;n<=i;n++)
+     //   printf("%d ",sta_table[n]);
+   // printf("%d\n",i+1);
+    
+    if(sta_table[0]==0)
+        sum=tonum(table[0]);
+        //printf("res=%d\n",sum);
+    for(n=0;n<=i;n++){
+        if(sta_table[n]!=0)
+        switch(*table[n]){
+                case '-':
+                sum-=tonum(table[n+1]);break;
+                default:
+                sum+=tonum(table[n+1]);
+                
+        }
+    }
+       sprintf(str,"res=%d",sum);
+    
 
-    for(n=0;n<=i;n++)
-        printf("%s ",table[n]);
-        printf("%d\n",i+1);
 	return 0;
 }
+int tonum(char* str)
+{
+    int res=0;
+    while(*str!='\0'){
+        res=res*10+*str-'0';
+        str++;
+     }
+     return res;
+}
+int gettype(char c)
+{
+    int sta;
+    if(c==' ')
+        return 9;
+    sta=(c>='0')?0:1;
+    return sta;
+}
 
+int chack(char* str)
+
+{
+    if(*str=='\0')
+        return 1;
+    while(*str!='\0'){
+        if((*str>'9')||(*str<' '))
+            return 1; 
+       str++; 
+    }
+    return 0;
+}
